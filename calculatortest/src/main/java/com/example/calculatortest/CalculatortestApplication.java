6c8587786b729b4adfa52eca13664f6198041248
SadoALikhan <khan.ali.99.sado@mail.ru>
2025-01-28 00:31:07 +0300

    Test

---
+++ calculatortest/src/main/java/com/example/calculatortest/CalculatortestApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatortestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatortestApplication.class, args);
+	}
+
+}
